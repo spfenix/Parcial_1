e683282e82e3388ff37e8c7816e24f323cf0f802
SPFENIX <cgofenix@hotmail.com>
2024-03-08 21:14:20 -0500

    Solicitud de datos por pantalla

---
+++ Algoritmo_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Un vendedor recibe un sueldo base de $XXXX.Además, recibe una comisión del 10 % sobre el valor total de cada venta que realiza.El vendedor desea saber:
+        //¿Cuánto dinero obtendrá por concepto de comisiones por las tres ventas que realiza en el mes
+        // ¿Cuál es el total que recibirá en el mes tomando en cuenta su sueldo base y comisiones?
+       // ¿Cuál fue la venta que le generó la mayor comisión?
+       //¿Cuál fue el promedio de las comisiones que recibió por cada venta?
+        //Además, la empresa da un beneficio extra de $100.000 si el vendedor supera el objetivo del mes. El objetivo es sumar en las 3 ventas como mínimo $1.000.000 en ventas. Mostrar un mensaje donde indique si ganó o no el beneficio
+        double sueldo, comi, vent1, vent2, vent3, totalvent, prom, benf;
+        Console.Write("Por favor digite el sueldo base: ");
+        sueldo = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite el valor de la primera venta: ");
+        vent1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite el valor de la segunda venta: ");
+        vent2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite el valor de la tercera venta: ");
+        vent3 = double.Parse(Console.ReadLine());
+         totalvent = 
+    }
+}
