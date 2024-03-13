94c7ab93d2c5751a25a1309f292a25714e4ac44e
SPFENIX <cgofenix@hotmail.com>
2024-03-12 19:57:26 -0500

    Solucion al ejercicio de los trenes!

---
+++ Algoritmo_tren2.0/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Un tren sale de la estación “A” a las 10:00 AM y viaja a una velocidad constante de 80 km/h. Un segundo tren sale de la misma estación a las 11:00 AM y viaja en la misma dirección a una velocidad constante de 100 km / h. ¿A qué hora el segundo tren alcanzará al primer tren?");
+
+        double tiempo, horafin;
+        tiempo = 80 / (100 - 80);
+        horafin = 11 + tiempo;
+
+        Console.WriteLine();
+
+        Console.WriteLine( " El segundo tren alcanza al primer tren a las " + horafin + " horas");
+    }
+}
